0dbe1dffef970c41f8b78e244c5eb53aa08b658c
Reinaldo\ReinaldoPicini <lpreinaldo@gmail.com>
2017-11-18 02:08:48 -0200

    Revert "Atualização de escopo"
    This reverts commit 3aaa2d5e2e2bdfcf616e4b2c6a4603f8e154499b.

---
+++ MeetingsPets/Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeetingsPets.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult CadUsuario()
+        {
+            ViewBag.Message = "Cadastro de Usuários";
+            return View();
+
+        }
+    }
+}
